a9bdd82afa4a1f7ab83910c32c46b392df060282
sanyomor-01 <sanyomormichael@gmail.com>
2023-11-02 21:07:53 +0000

    react image added and max-widthset to 1000px

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,11 +1,12 @@
 import React from "react"
+import logo from "../images/react-icon-small.png"
 
 
 export default function Navbar() {
     return (
         <nav className="nav">
             <div className="logo-brand">
-                <img src="{../assets/reactjs-icon.png}" alt="react png" />
+                <img src={logo} alt="react png" width="40px" />
                 <span>ReactFacts</span>
             </div>
             <h3>React Course -Project 1</h3>
