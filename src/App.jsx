7d0f6077320e1a65210bdd70d2c6232150459812
sanyomor-01 <sanyomormichael@gmail.com>
2023-11-02 20:50:13 +0000

    simple react webpage

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+import Navbar from "./Components/Navbar"
+import Main from "./Components/Main"
+import "./App.css"
+
+export default function App(){
+    
+        return(
+            <div className="container">
+                <Navbar/>
+                <Main/>
+            </div>
+        );
+    
+}
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+
+export default function Navbar() {
+    return (
+        <nav className="nav">
+            <div className="logo-brand">
+                <img src="{../assets/reactjs-icon.png}" alt="react png" />
+                <span>ReactFacts</span>
+            </div>
+            <h3>React Course -Project 1</h3>
+        </nav>
+    )
+}
